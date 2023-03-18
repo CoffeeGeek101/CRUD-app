3afb59cb28823ffae5ef99e12d7333f8898553b5
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-03-18 11:02:42 +0530

    responsive v1.02

--- src/component/TaskList.tsx
+++ src/component/TaskList.tsx
@@ -62,7 +62,7 @@ export const TaskList = () => {
             if (priorityDecision !== 0) {
                 return priorityDecision;
             }
-            const dateA:any = a.date ? new Date(a.date).getTime() : 0;
+            const dateA = a.date ? new Date(a.date).getTime() : 0;
             const dateB = b.date ? new Date(b.date).getTime() : 0;
             return dateA - dateB;
         }
